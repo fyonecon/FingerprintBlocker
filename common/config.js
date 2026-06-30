65c5ca0a82f13c054936cac625d0e2a1b9c39efc
fy <fyonecon@gamil.com>
2026-06-30 12:04:03 +0800

    规范提交审核时的多语言

--- common/config.js
+++ common/config.js
@@ -1,6 +1,6 @@
 const config = {
     app_name: "FingerprintBlocker", // 应用名
     app_class: "fingerprint_blocker_", // 必须是字母、数字、下划线、非数值开头
-    app_version: "1.0.9", // 版本号 1.0.0
+    app_version: "1.0.10", // 版本号 1.0.0
     console_debug: false, // 显示console日志。false true
 };
