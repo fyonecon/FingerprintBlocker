d0703eaf2ac115a123eb87ee9e4ab11c6be9d051
fy <fyonecon@gamil.com>
2026-07-07 17:51:52 +0800

    update

--- common/config.js
+++ common/config.js
@@ -3,5 +3,5 @@ const config = {
     app_class: "fingerprint_blocker_", // 必须是字母、数字、下划线、非数值开头
     app_version: "1.0.12", // 版本号 1.0.0
     console_debug: false, // 显示console日志。false true
-    storage_type: "local", // 可选: "sync" | "local"
+    storage_type: "local", // 可选: "sync" | "local"，默认 local
 };
